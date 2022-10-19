5b8527ea05dc17a21ce7773b022b9f078da1f559
Peterhamz <106712371+Peterhamz@users.noreply.github.com>
2022-10-19 20:02:36 +0100

    Bank Application...

---
+++ BankingApplication/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount("Peter James", "ki909");
+        account1.showMenue();
+
+
+    }
+}
